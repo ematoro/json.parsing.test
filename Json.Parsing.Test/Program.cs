46918bed6a6fe7633137dcb5cd4d43d61285a2b5
Emanuel Toro <etoro@sancorseguros.com>
2023-11-03 11:37:38 -0300

    pruebas de  Using JObject

--- Json.Parsing.Test/Program.cs
+++ Json.Parsing.Test/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.Linq;
 
 namespace Json.Parsing.Test
 {
@@ -6,8 +9,39 @@ namespace Json.Parsing.Test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hola mundo!");
+            Console.WriteLine("Reading data from json file...");
+            var myJsonString = File.ReadAllText("myfile.json");
+            Console.WriteLine("The data read is:");
+            Console.WriteLine(myJsonString);
+            Console.WriteLine();
+            //Analizando el contenido del json
+            objectParse(myJsonString);
+
+            //Salir del programa
+            Console.WriteLine("Press enter to exit");
             Console.ReadLine();
+        }
+
+        /*
+         * Un método para analizar el contenido: Using JObject
+         */
+        private static void objectParse(string myJsonString)
+        {
+            var myJObject = JObject.Parse(myJsonString);
+            if (myJObject.SelectToken("MyStringProperty") != null)
+            {
+                var value = myJObject.SelectToken("MyStringProperty").Value<string>();
+                Console.WriteLine("Parser e.g. 1: " + value);
+            }
+            else
+            {
+                Console.WriteLine("Wrong parsing, the token was not found.");
+            }
+            //Otros ejemplos usando el query language JSON
+            var mySubDocumentValue = myJObject.SelectToken("$.MySubDocument.SubDocumentProperty").Value<string>();
+            Console.WriteLine("Parser e.g. 2: " + mySubDocumentValue);
+            var myList = myJObject.SelectTokens("$.MyListProperty").Values<int>().ToList();
+            Console.WriteLine("Parser e.g. 3: " + myList[1].ToString());
         }
     }
 }
---
+++ Json.Parsing.Test/README.md
@@ -0,0 +1,5 @@
+# Cómo analizar JSON en C# .NET
+Estaré usando una aplicación de consola para probar diferentes formas de analizar el contenido de una archivo simple.
+Seguiré los ejemplos y explicaciones del siguiente artículo:
+[Enlace](https://dotnetcoretutorials.com/how-to-parse-json-in-c-net/)
+##Usando JObject
